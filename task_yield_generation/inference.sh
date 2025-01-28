5fbb7541c2110f79d07aeed830b164f33edd97f1
sagawatatsuya <zdaye0969@gmail.com>
2025-01-28 15:01:54 +0900

    delete files

--- task_yield_generation/inference.sh
+++
@@ -1,8 +0,0 @@
-CUDA_VISIBLE_DEVICES=2,3 python prediction.py \
-    --input_data_YIELD="/data1/ReactionT5_neword/data/C_N_yield/MFF_Test1/test.csv" \
-    --input_max_length="150" \
-    --model_name_or_path="/data1/ReactionT5_neword/task_yield_generation/400epoch/checkpoint-19008" \
-    --num_beams=5 \
-    --num_return_sequences=5 \
-    --batch_size=12 \
-    --output_dir="./"
